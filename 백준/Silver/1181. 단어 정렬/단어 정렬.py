cf88a54f6af78c9a6682ff4eea72fe1e8d3ce9bd
ms <altjsvnf@naver.com>
2022-05-14 00:02:35 +0900

    add: comment

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -5,7 +5,7 @@ for i in range(N):
     arr.append(input())
 
 my_set = set(arr)
-my_list = list(my_set)
+my_list = list(my_set) #집합은 sort함수를 쓰지 못하기 때문에 list로 다시 바꿔준다.
 my_list.sort()
 my_list.sort(key=len)
 
