9de5879139c45581096ab4acc15c158e87ae5b05
MiSun <97578390+KangMiSun17@users.noreply.github.com>
2025-05-05 18:21:29 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
@@ -1,47 +1,21 @@
-//한글자
-var fs = require('fs');
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-var input = fs.readFileSync('/dev/stdin').toString();
-
-// const input1 = input.trim();
-
-// 한 줄에 여러 글자
-// const input2 = input.split(' ');
-
-// 여러 줄
-
-const input3 = input.trim().split('\n');
-
-const inputValue = input3;
-const N = inputValue[0];
+const n = input[0];
 let count = 0;
-let items = {};
 
-for (i = 0; i < N; i++) {
-  items = {};
-
-  for (j = 0; j < inputValue[i + 1].length; j++) {
-    if (items[inputValue[i + 1][j]]) {
-      items[inputValue[i + 1][j]].push(j);
-    } else {
-      items[inputValue[i + 1][j]] = [j];
+for(let i = 1; i <= n; i++){
+    const word = input[i];
+    let boolean = true;
+    
+    for(let j = 0; j <word.length; j++){
+        if(word.indexOf(word[j]) !== j){
+            if(word[j] !== word[j-1]){
+                boolean = false;
+                break;
+            }
+        }
     }
-  }
-
-  const correct = Object.values(items).every(item => {
-    const isOneGap = item.every((value, index) => {
-      if (index === 0) {
-        return true;
-      }
-      return value - item[index - 1] === 1;
-    });
-
-    if (item.length === 1 || isOneGap) {
-      return true;
-    }
-  });
-
-  if (correct) count++;
+    if(boolean) count++;
 }
-
 console.log(count);
