8678611a4afc4057907f09059400ef52e1b43af1
MiSun <97578390+KangMiSun17@users.noreply.github.com>
2026-04-25 15:39:41 +0900

    [level 0] Title: 코드 처리하기, Time: 34.13 ms, Memory: 39.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,26 @@
+function solution(code) {
+    var answer = '';
+    let mode = 0;
+    
+    for (let i = 0; i < code.length; i++) {
+        if(mode === 0){
+            if(code[i] === "1"){
+                mode = 1;
+            } else {
+                if(i % 2 === 0){
+                    answer += code[i];
+                }
+            }
+        } else {
+            if(code[i] !== "1" ) {
+                if(i % 2 !== 0){
+                    answer += code[i];
+                }
+            } else {
+                mode = 0;
+            }
+        }
+    }
+        
+    return answer.length ? answer : "EMPTY";
+}
